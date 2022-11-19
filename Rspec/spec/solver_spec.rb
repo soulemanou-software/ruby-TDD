f6a960d830d96b05a1237adde5df3b2343d628cb
soulemanou kunchiefuh babanou <57832090+soulemanou-software@users.noreply.github.com>
2022-11-19 22:21:46 +0100

    testing of the tdd project complited succesfully

--- Rspec/spec/solver_spec.rb
+++ Rspec/spec/solver_spec.rb
@@ -4,6 +4,8 @@ describe Solver do
   context 'When testing the Solver class' do
     it 'should return the factorial of number passed to the factorial method of class Solver' do
       solver = Solver.new
+      expect(solver.factorial(0)).to eq 1
+      expect(solver.factorial(1)).to eq 1
       expect(solver.factorial(5)).to eq 120
       expect(solver.factorial(3)).to eq 6
       expect(solver.factorial(4)).to eq 24
--- solver.rb
+++ solver.rb
@@ -2,7 +2,7 @@ class Solver
   def fizzbuzz(num)
     if (num % 3).zero? && (num % 5).zero?
       'fizzbuzz'
-    elsif (num % 3).zero
+    elsif (num % 3).zero?
       'fizz'
     elsif (num % 5).zero?
       'buzz'
@@ -12,14 +12,14 @@ class Solver
   end
 
   def factorial(num)
-    return 1 if num.zero?
-
-    num * factorial(num - 1)
+    if num < 1
+      1
+    else
+      num * factorial(num - 1)
+    end
   end
 
   def reverse(str)
-    return str[0] if str.length == 1
-
-    reverse(str.slice(1, str.length - 1)) + str[0]
+    str.reverse
   end
 end
