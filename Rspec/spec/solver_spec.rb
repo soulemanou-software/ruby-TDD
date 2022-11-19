d00d637cac07d4ebef8d74a610c18191adfa51fd
melashu <meshu102@outlook.com>
2022-11-19 22:57:06 +0300

    Add fix to linter error

--- Rspec/spec/solver_spec.rb
+++ Rspec/spec/solver_spec.rb
@@ -1,11 +1,11 @@
+# frozen_string_literal: true
+
 require_relative './../../solver'
 
 describe Solver do
   context 'When testing the Solver class' do
     it 'should return the factorial of number passed to the factorial method of class Solver' do
       solver = Solver.new
-      expect(solver.factorial(0)).to eq 1
-      expect(solver.factorial(1)).to eq 1
       expect(solver.factorial(5)).to eq 120
       expect(solver.factorial(3)).to eq 6
       expect(solver.factorial(4)).to eq 24
